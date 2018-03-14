266e44d7fec824a5ac93aac3acd437b242ad76a4
quan <quanluis@protonmail.com>
2018-03-14 19:47:54 -0400

    created helper function to organize an array of products into a object of products

--- routers/products.js
+++ routers/products.js
@@ -1,10 +1,30 @@
 const express = require('express');
 const router = express.Router();
 const mockProducts = require('../mocks/products');
+const productArrToObj = (arrayOfProducts) => {
+    // create an accumulator obj 
+    const accumulator = {};
+    // for each product in array of products 
+    arrayOfProducts.forEach(product => {
+        const id = product._id;
+        const copy = {...product}
+        delete copy._id;
+        accumulator[id] = copy;
+        
+    });
+       //grab the id 
+
+       //delete the _id internal to the object 
+       // set the id val in the accumulato obj equal to product  
+    //return accumulator 
+    return accumulator;
+
+
+}
 
 router.get('/products', (req, res) => {
     res.status(200).json({
-        products: mockProducts
+        products: productArrToObj(mockProducts)
     })
 });
 
