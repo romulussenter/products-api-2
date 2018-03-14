9d9f9e1fb9f8d5709ccd0e15be6aca1b7a42fc6a
quan <quanluis@protonmail.com>
2018-03-13 20:56:12 -0400

    added nodemon to project

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const serverApp = express();
 const PORT = process.env.PORT || 5000; // necessary for huroku deployment
 
 serverApp.get('/', (req, res) => {
-     res.send('hello i work!!');
+     res.send('AMAZING');
 });
 
 serverApp.listen(PORT, () => {
