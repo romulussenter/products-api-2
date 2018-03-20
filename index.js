f19b6a39f32ab52410ab164a58cf4555d8c6ab03
Romulus <romulus.senter@yahoo.com>
2018-03-19 20:35:38 -0400

    Wired up body-parsuer for logging

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 
 require('dotenv').config();
 //middleware imports
+const bodyParser =require('body-parser');
 const logger =require('./middlewares/logger');
 const  notFound =require('./middlewares/404');
 const serverError = require('./middlewares/serverError');
@@ -16,13 +17,15 @@ const PORT = process.env.PORT || 5000; // necessary for huroku deployment
 //Routers   
 
 const productRouter = require('./routers/products');
-
+serverApp.use(bodyParser.json())
+serverApp.use(bodyParser.urlencoded({ extended: true}));
 serverApp.use(logger);
 serverApp.use(productRouter); //register the router with the application
 
 serverApp.get('/', (req, res) => {
      res.send('AMAZING');
 });
+
 serverApp.use(notFound);
 serverApp.use(serverError);
 
